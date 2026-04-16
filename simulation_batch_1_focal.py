611a113a6bb5e303bc9b2b164d925455f590fc73
jiyuehe <jiyue_he@163.com>
2026-04-15 23:41:21 -0700

    update simulation_batch_1_focal.py

--- simulation_batch_1_focal.py
+++ simulation_batch_1_focal.py
@@ -13,75 +13,74 @@
 # limitations under the License.
 
 #%%
-import os
-from pathlib import Path
-script_dir = os.path.dirname(os.path.abspath(__file__)) # get the path of the current script
-os.chdir(script_dir) # change the working directory
-script_dir = Path(script_dir)
-
-# add the workspace root to Python path
-import sys
-workspace_root = Path().resolve().parent # Path().resolve() returns an absolute path, the full path
-if str(workspace_root) not in sys.path:
-    sys.path.insert(0, str(workspace_root))
-import common
-
-import simulation
-import toolbox.codes
-import simulation_individual
 import numpy as np # pip install numpy
-from scipy.signal import find_peaks
-import matplotlib.pyplot as plt # pip install matplotlib
-
-import utility.lat_map as lat_map
+import os
+import simulation_individual
+import utility
+import configuration
 
 #%% 
-directory = {}
-directory['home'] = script_dir
-directory['result'] = Path('/home/j/Desktop/hdd/103_1-lagood_3mm_1focal')
-directory['data'] = script_dir.parent / '0_data'
+directory = configuration.directory_setup() # set up directories
+name_prefix = configuration.mesh_name() # get mesh name prefix
 
-# create the folder if it does not exist
-directory['result'].mkdir(exist_ok=True)
-
-geometry_name = '103_1-lagood_geometry.npz'
+n_simulations = 20
+save_result_flag = 1 # 1: save simulation results, 0: do not save simulation results
+plot_lat_map_flag = 1 # 1: plot local activation time map. 0: do not plot local activation time map
 
 # load geometry data
-file_path = directory['data'] / geometry_name
+file_path = directory['data'] / f'{name_prefix}_geometry.npz'
 data = np.load(file_path, allow_pickle=False)
 geometry_data = {k: data[k] for k in data.files}
 
-simulation_parameters = simulation.setting.assign_simulation_parameters(geometry_data)
+voxel = geometry_data['voxel']
+n_voxel = voxel.shape[0]
+electrode_voxel = voxel[geometry_data['voxel_id_of_electrode'], :]
+
+voxel_id_of_electrode = geometry_data['voxel_id_of_electrode']
+s1 = np.random.choice(voxel_id_of_electrode, size=n_simulations, replace=False) # random ids in voxel_id_of_electrode without replacement
+s2 = []
+
+simulation_parameters, arrhythmia_parameters, heart_model_parameters = configuration.assign_simulation_parameters(name_prefix, geometry_data, s1, s2, n_voxel)
 
 input_arguments = {}
+input_arguments['name_prefix'] = name_prefix
 input_arguments['geometry_data'] = geometry_data
-input_arguments['save_result_flag'] = 1
+input_arguments['save_result_flag'] = save_result_flag
 input_arguments['result_folder'] = directory['result']
 input_arguments['simulation_parameters'] = simulation_parameters
-
-# save s1 and s2 to text file
-n_simulations = 3000
-voxel_id_of_voxel3mm = geometry_data['voxel_id_of_voxel3mm']
-
-s1 = np.random.choice(voxel_id_of_voxel3mm, size=n_simulations, replace=False) # random ids in voxel_id_of_voxel3mm without replacement
+input_arguments['arrhythmia_parameters'] = arrhythmia_parameters
+input_arguments['heart_model_parameters'] = heart_model_parameters
 
 #%%
-plot_lat_map_flag = 1
-
-geometry = {}
-geometry['node'] = geometry_data['voxel'][voxel_id_of_voxel3mm, :]
-
+# run simulations
 for loop_id in range(n_simulations): # 0 to n_simulations-1
     print(f'===== simulation set {loop_id+1} of {n_simulations} =====')
 
-    focal_1 = s1[loop_id]
-    focal_2 = []
-    if not os.path.exists(directory['result'] / f'lat_{str(focal_1)}.npz'):
-        input_arguments['s1'] = focal_1
-        input_arguments['s2'] = focal_2
+    if not os.path.exists(directory['result'] / f'lat_{str(s1[loop_id])}.npz'):
+        input_arguments['s1'] = s1[loop_id]
+        input_arguments['s2'] = s2
         simulation_individual.run_simulation(input_arguments)
-        
-    lat_map.execute(geometry, directory['result'], focal_1, focal_2, plot_lat_map_flag)
+
+        # compute local activation time
+        file_name = f'{name_prefix}_simulation_results_{s1[loop_id]}.npz'
+        simulation_results = dict(np.load(directory['result'] / file_name, allow_pickle=False)) # load simulation results
+        electrogram_unipolar = simulation_results['electrogram_unipolar']
+        lat_electrode = utility.lat_map.compute_electrode_lat(electrogram_unipolar)
+
+        # interpolate local activation time from electrode locations to all voxels
+        lat_voxel = utility.lat_map.interpolate_lat(voxel, electrode_voxel, lat_electrode)
+
+        # plot local activation time map
+        if plot_lat_map_flag == 1:
+            fig_name = directory['result'] / f'{name_prefix}_lat_{str(s1[loop_id])}.png'
+            
+            geometry_flag = simulation_results['geometry_flag']
+            utility.lat_map.plot(voxel, lat_voxel, geometry_flag, fig_name)
+            utility.common.crop_image(fig_name)
+
+        # save lat to simulation_results
+        simulation_results['lat_electrode'] = lat_electrode
+        np.savez(directory['result'] / file_name, **simulation_results)
 
 #%%
 print('done')
