6b7693452ad49a37b31c8738d6f8fb3961da6d28
jiyuehe <jiyue_he@163.com>
2026-04-23 12:30:30 -0700

    .

--- utility/automatic_mesh_refinement.py
+++ utility/automatic_mesh_refinement.py
@@ -1,4 +1,4 @@
-# Copyright 2026 Jiyue He
+# Copyright 2026 Mason Manetta
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
