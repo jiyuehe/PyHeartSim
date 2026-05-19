c63f2acd18fa960102f877fbca8a403b36e202bc
jiyuehe <jiyue_he@163.com>
2026-05-19 14:56:56 -0700

    .

--- simulation_batch_1_focal.py
+++ simulation_batch_1_focal.py
@@ -21,7 +21,7 @@ import common
 import configuration
 
 #%% 
-mesh_id = 7 
+mesh_id = 87
 name_prefix = configuration.mesh_name(mesh_id) # get mesh name prefix
 directory = configuration.directory_setup() # set up directories
 
